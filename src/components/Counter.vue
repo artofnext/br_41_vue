f3f096d42fa40dfb672d794b44c1163f701e0168
artofnext <novnovalex@gmail.com>
2019-12-11 20:40:16 +0200

    add method

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,11 +1,23 @@
 <template>
     <div>
-        Counter
+        <h1>{{ counter }}</h1>
+        <br>
+        <button v-on:click="increaseCounter">Increase</button>
     </div>
 </template>
 
 <script>
 export default {
     name: "Counter",
+    data() {
+        return {
+            counter: 0,
+        }
+    },
+    methods: {
+        increaseCounter() {
+            this.counter++;
+        }
+    }
 }
 </script>
